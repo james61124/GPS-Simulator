9a2c2d60dbba7173d7c31ed0aa7066df1c3a3816
james61124 <james61124@gmail.com>
2026-02-01 14:58:59 -0600

    Initial Commit

---
+++ GPS_Demo/GPS_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPS_DemoApp.swift
+//  GPS_Demo
+//
+//  Created by james on 2026/2/1.
+//
+
+import SwiftUI
+
+@main
+struct GPS_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
