c91968516613b52d696b8cb4f3f2d1b6f6c031df
moirachen1019 <peggy10191019@gmail.com>
2026-02-04 01:27:07 -0600

    disabled health steps feature

--- GPS_Demo/ContentView.swift
+++ GPS_Demo/ContentView.swift
@@ -2,12 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Writing 20 steps every 10 seconds")
-            .onAppear {
-                HealthManager.shared.requestHealthAuth()
-                HealthManager.shared.startWritingFixedSteps()
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
     }
+    // var body: some View {
+    //     Text("Writing 20 steps every 10 seconds")
+    //         .onAppear {
+    //             HealthManager.shared.requestHealthAuth()
+    //             HealthManager.shared.startWritingFixedSteps()
+    //         }
+    // }
 }
 
 #Preview {
